b803721b68abb0c8a147d3fd773711bf11e19bf3
byrmgng <byrmgng@gmail.com>
2024-06-25 20:30:54 +0300

    MVC-AZURE-TTS
    MVC AZURE TTS APPLICATION

---
+++ TextToSpeech/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TextToSpeech.Models;
+
+namespace TextToSpeech.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            SesSentezleme sesSentezleme = new SesSentezleme();
+            string[] tarayiciDilleri = Request.Headers["Accept-Language"].ToString().Split(',');
+            sesSentezleme.Dil = tarayiciDilleri[0];
+            return View(sesSentezleme);
+        }
+
+        [HttpPost]
+        public IActionResult Index(SesSentezleme sesSentezleme)
+        {
+            _ = sesSentezleme.MetinOkumaAsync();
+            return View(sesSentezleme);
+        }
+        [HttpPost]
+        public IActionResult Durdur(SesSentezleme sesSentezleme)
+        {
+            sesSentezleme.MetinOkumayiDurdur();
+            return Ok();
+        }
+        [HttpPost]
+        public IActionResult SesKaydet(SesSentezleme sesSentezleme, string dosyaTuru)
+        {
+            if( !dosyaTuru.Equals("mp3") && !dosyaTuru.Equals("wav"))
+            {
+                dosyaTuru = "mp3";
+            }
+            if(sesSentezleme.Metin == null)
+            {
+                sesSentezleme.Metin = "";
+            }
+            Guid myuuid = Guid.NewGuid();
+            string myuuidAsString = myuuid.ToString();
+            var dosyaAdi = myuuidAsString + '.' +dosyaTuru;
+            sesSentezleme.SesDosyasiKaydet(dosyaAdi).Wait();
+            var yol = Path.Combine(Directory.GetCurrentDirectory(), dosyaAdi);
+            var bellek = new MemoryStream();
+            using (var stream = new FileStream(yol, FileMode.Open))
+            {
+                stream.CopyTo(bellek);
+            }
+            bellek.Position = 0;
+            System.IO.File.Delete(yol);
+            return File(bellek, "audio/"+dosyaTuru, dosyaAdi);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TextToSpeech/Models/SesSentezleme.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using static System.Net.Mime.MediaTypeNames;
+namespace TextToSpeech.Models
+{
+    public class SesSentezleme
+    {
+        private static string abonelikAnahtari = "API_KEY";
+        private static string abonelikBolgesi = "API_REGION";
+        static SpeechSynthesizer sentezleyici = null;
+        public string Metin { get; set; }
+        public string Dil { get; set; }
+        public string Konusmaci { get; set; }
+
+        public async Task MetinOkumaAsync()
+        {
+            var yapilandirma = SpeechConfig.FromSubscription(abonelikAnahtari, abonelikBolgesi);
+            yapilandirma.SpeechSynthesisLanguage = Dil;
+            yapilandirma.SpeechSynthesisVoiceName = Konusmaci;
+            using (sentezleyici = new SpeechSynthesizer(yapilandirma))
+            {
+                var sonuc = await sentezleyici.SpeakTextAsync(Metin);
+
+                if (sonuc.Reason == ResultReason.SynthesizingAudioCompleted)
+                {
+                    Console.WriteLine($"Metin Seslendirildi");
+                }
+                else if (sonuc.Reason == ResultReason.Canceled)
+                {
+                    var iptal = SpeechSynthesisCancellationDetails.FromResult(sonuc);
+                    Console.WriteLine($"Seslendirme iptal edildi: Sebep ={iptal.Reason}");
+
+                    if (iptal.Reason == CancellationReason.Error)
+                    {
+                        Console.WriteLine($"Seslendirme iptal edildi: Hata Kodu - Detay ={iptal.ErrorCode}\n{iptal.ErrorDetails}");
+                    }
+                }
+            }
+        }
+        public void MetinOkumayiDurdur()
+        {
+            sentezleyici.StopSpeakingAsync();
+        }
+        public static async Task<List<VoiceInfo>> SesSecenekleri()
+        {
+            var yapilandirma = SpeechConfig.FromSubscription(abonelikAnahtari, abonelikBolgesi);
+            List<VoiceInfo> sesListesi = new List<VoiceInfo>();
+            using (var sentezleyici = new SpeechSynthesizer(yapilandirma))
+            {
+                using (var sonuc = await sentezleyici.GetVoicesAsync())
+                {
+                    if (sonuc.Reason == ResultReason.VoicesListRetrieved)
+                    {
+                        Console.WriteLine("Ses seçenekleri getirildi.");
+                        sesListesi = sonuc.Voices.ToList();
+                    }
+                    else if (sonuc.Reason == ResultReason.Canceled)
+                    {
+                        Console.WriteLine($"İptal Edildi : Detay=[{sonuc.ErrorDetails}]");
+                    }
+                }
+            }
+            return sesListesi;
+        }
+        public async Task SesDosyasiKaydet(string dosyaAdi)
+        {
+            var yapilandirma = SpeechConfig.FromSubscription(abonelikAnahtari, abonelikBolgesi);
+            yapilandirma.SpeechSynthesisLanguage = Dil;
+            yapilandirma.SpeechSynthesisVoiceName = Konusmaci;
+            using (var dosyaCiktisi = AudioConfig.FromWavFileInput(dosyaAdi))
+            using (var sentezleyici = new SpeechSynthesizer(yapilandirma, dosyaCiktisi))
+            {
+                using (var sonuc = await sentezleyici.SpeakTextAsync(Metin))
+                {
+                    if (sonuc.Reason == ResultReason.SynthesizingAudioCompleted)
+                    {
+                        Console.WriteLine($"Ses dosya gönderim işlemi başarılı");
+                    }
+                    else if (sonuc.Reason == ResultReason.Canceled)
+                    {
+                        var iptal = SpeechSynthesisCancellationDetails.FromResult(sonuc);
+                        Console.WriteLine($"Dosya indirme işlemi iptal edildi: Sebep={iptal.Reason}");
+
+                        if (iptal.Reason == CancellationReason.Error)
+                        {
+                            Console.WriteLine($"Seslendirme iptal edildi: Hata Kodu - Detay ={iptal.ErrorCode}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
